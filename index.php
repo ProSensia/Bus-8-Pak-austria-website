b786b78b465b4571cd948620bb0f3db48c12610e
ProSensia <prosensia@gmail.com>
2025-11-16 18:25:08 +0500

    fixed version of styling for cache

--- index.php
+++ index.php
@@ -192,7 +192,7 @@ if ($result->num_rows > 0) {
     <title>Coaster Bus Seat Booking</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=1.0">
 
 </head>
 <body>
