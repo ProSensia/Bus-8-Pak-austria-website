dbbeef1bab75423d02f3e5ea8e85b09b630cda79
ProSensia <prosensia@gmail.com>
2025-11-16 18:39:27 +0500

    Updated css style v to 2.0

--- index.php
+++ index.php
@@ -193,7 +193,7 @@ if ($result->num_rows > 0) {
     <title>Coaster Bus Seat Booking</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="style.css?v=1.0">
+    <link rel="stylesheet" href="style.css?v=2.0">
 
 </head>
 
